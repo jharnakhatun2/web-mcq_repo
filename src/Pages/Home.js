f6943d7ba8c2c0fa7098976e55d9deab2c6a1572
Jharna203 <jharnakhatun203@gmail.com>
2022-10-11 01:10:07 +0600

    added home page header section

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,9 +1,23 @@
+import { BsArrowRight } from "react-icons/bs";
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import img from './background.png';
+
+
 
 const Home = () => {
     return (
         <div>
-            <h1>Home Page</h1>
+        <Container className="headerStyle">
+            <div className="headingText">
+            <h1 className="headingSize">Play funny learning by <span className="hightlightColor">Quiz</span>!</h1>
+            <p className="paragraphText">If learning is fun, we learn better.  Quiz can be a few minutes of relaxation from our hours of marking. So, if we enjoy it, why wouldn’t web developer, we learn also welcome a little bit of relaxing fun whilst working? Start to identify gaps in your knowledge. This is the Ideal System of revision. </p>
+            <Link to='/blogs' className="cardFooter">Learn More <BsArrowRight/> </Link>
+            </div>
+            <img  className="mw-100" src={img} alt="" />
+        </Container>
+            
         </div>
     );
 };
