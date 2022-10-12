07fdd8615b81e9bd3cd1615d5d9d87b72ac9cb1e
Jharna203 <jharnakhatun203@gmail.com>
2022-10-12 10:30:58 +0600

    added ui responsive for home route

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -2,7 +2,7 @@ import { BsArrowRight } from "react-icons/bs";
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import { Link, useLoaderData } from 'react-router-dom';
-import img from './background.png';
+import img from './profile.png';
 
 
 
@@ -15,24 +15,34 @@ const Home = () => {
     return (
         <div>
             <Container className="headerStyle">
-                <div className="headingText">
-                    <h1 className="headingSize">Play funny learning by <span className="hightlightColor">Quiz</span>!</h1>
-                    <p className="paragraphText">If learning is fun, we learn better.  Quiz can be a few minutes of relaxation from our hours of marking. So, if we enjoy it, why wouldn’t web developer, we learn also welcome a little bit of relaxing fun whilst working? Start to identify gaps in your knowledge. This is the Ideal System of revision. </p>
-                    <Link to='/blogs' className="cardFooter">Learn More <BsArrowRight /> </Link>
+                {/* Quiz Search Area */}
+                <div className="search-topics shadow-sm">
+                    <h1 className="topicHeader">What will you teach today?</h1>
+                    <input className="search" type="text" placeholder="Search for quizzes on bellow topics" />
                 </div>
-                <img className="mw-100" src={img} alt="" />
+                {/* Quiz Profile Part */}
+                <div className="profile shadow-sm text-center">
+                    <img className="profile-img" src={img} alt="" />
+                    <h5>Create an account</h5>
+                    <div className="sign-part">
+                        <a className="mx-2 text-decoration-none" href="#">Sign Up</a>
+                        <a className="mx-2 text-decoration-none" href="#">Sign In</a>
+                    </div>
+                </div>
             </Container>
+            <h2 className="section-header">Choice By Topics</h2>
             <section>
                 {
                     quizData.data.map((quiz) => {
                         const { id, name, logo, total } = quiz;
-                        return <article className="card-style" key={id}>
-                            <div className="imageDiv">
-                                <img className="img-fluid" src={logo} alt="" />
+                        return <article  key={id}>
+                            <div className="card border-0 shadow-lg">
+                                <img className="img-fluid bg-dark " style={{height:'303px'}} src={logo} alt="" />
+                            
+                            <div className="card-body  d-flex justify-content-between align-items-center">
+                                <Link to={`/topics/${id}`} className="cardFooter text-decoration-none">{name}</Link>
+                                <button type="button" className="btn btn-success"><Link className ="text-white text-decoration-none" to={`/quiz/${id}`}>Start Quiz</Link></button>
                             </div>
-                            <div className="cardBottom">
-                                <Link to={`/topics/${id}`} className="cardFooter">{name}</Link>
-                                <button className="cardFooter"><Link to={`/quiz/${id}`}>Start Practice</Link></button>
                             </div>
                         </article>
                     })
